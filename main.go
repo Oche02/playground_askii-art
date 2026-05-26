7508644392a73b60edeaebf4990b8840745b27db
Augustine Oche <augustineoche02@gmail.com>
2026-05-26 15:00:33 +0000

    added function that can successfully validate my banner file

--- main.go
+++ main.go
@@ -1,2 +1,16 @@
 package main
 
+func validateBanner(banner map[rune][]string)error{
+	if len(banner) != 95 {
+		return fmt.Errorf("invalid banner")
+	}
+	for key, value := range banner {
+		if key < 32 || key >126 {
+			return fmt.Errorf("invalid character")
+		}
+		if len(value) != 8 {
+			return fmt.Errorf("invalid length")
+		}
+	}
+	return nil
+}
