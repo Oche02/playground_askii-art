fb87a559dd3b222e20ef39683bb78a4319a4655e
Augustine Oche <augustineoche02@gmail.com>
2026-05-26 15:16:25 +0000

    added a function capable to medge two mapto return one map

---
+++ medgeBanner.go
@@ -0,0 +1,13 @@
+package main
+
+func medgeBanner(base priority map[rune][]string)map[rune][]string{
+	result := map[rune][]string{}
+	
+	for key, value := range base{
+		result[key] = value
+	}
+	for key, value := range priority{
+		result[key] = value
+	}
+	return result
+}
